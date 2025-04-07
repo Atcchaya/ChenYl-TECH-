c3295339556e27f7b1a1b087b149faeb56ab5444
Yemia-23 <yehouenoum@cy-tech.fr>
2025-04-07 11:33:48 +0200

    Update main.h

--- main.h
+++ main.h
@@ -4,5 +4,16 @@
 #include <string.h>
 
 
-
-
+typedef struct {
+    int nb_id;//numéro d'identification
+    char nom;
+    char espèces[30];//à revoir
+    int annee;
+    int poids;
+    char com[100];//commentaire
+    }animal;
+    
+typedef struct {
+    animal Animal[50];
+    int nb_animal;//nombre d'animaux présents dans le refuge
+    }refuge;
