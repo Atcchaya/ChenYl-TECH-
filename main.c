b8c868460e79ab3c9d30125f1fbb6383592c1386
Yemia_23 <yehouenoum@cy-tech.fr>
2025-04-10 09:12:09 +0200

    Modif
    Modification

--- main.c
+++ main.c
@@ -5,48 +5,6 @@ void ajouterAnimal();
 void rechercherAnimal();
 void adopterAnimal();
 
-int main() {
-    int choix;
-
-    do {
-        printf("\n--- Menu ChenYl-Tech ---\n");
-        printf("1. Ajouter un animal\n");
-        printf("2. Rechercher un animal\n");
-        printf("3. Adopter un animal\n");
-        printf("4. Quitter\n");
-        printf("Votre choix : ");
-        scanf("%d", &choix);
-
-        switch (choix) {
-            case 1:
-                ajouterAnimal();
-                break;
-            case 2:
-                rechercherAnimal();
-                break;
-            case 3:
-                adopterAnimal();
-                break;
-            case 4:
-                printf("Au revoir !\n");
-                break;
-            default:
-                printf("Choix invalide. Veuillez réessayer.\n");
-        }// Afficher menu
-
-    } while (choix != 4);
-
-    return 0;
-}
-
-
-#include <stdio.h>
-
-// Prototypes des fonctions (on les écrira plus tard)
-void ajouterAnimal();
-void rechercherAnimal();
-void adopterAnimal();
-
 // Fonction pour afficher le menu dans un fichier
 void afficherMenu(FILE *fichier) {
     fprintf(fichier, "\n--- Menu ChenYl-Tech ---\n");
